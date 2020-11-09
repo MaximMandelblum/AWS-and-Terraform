19afa1bab1b6db820b257a30e83438f40d9d7e4e
maximm <maxim.mandelblum@imperva.com>
2020-11-09 09:46:14 +0200

    added cronjob for copy

--- Homework4/user_date.tf
+++ Homework4/user_date.tf
@@ -8,5 +8,9 @@ sudo apt install nginx -y
 sed -i 's/nginx/OpsSchool userip: '$IP' great you here with us!!/g' /var/www/html/index.nginx-debian.html
 sed -i '15,23d' /var/www/html/index.nginx-debian.html
 service nginx restart
+
+sudo apt install awscli -y
+(crontab -l && echo "* 1 * * * /usr/bin/aws s3 cp /var/log/nginx/access.log s3://maxim-1983-1983") | crontab -
+
 USERDATA
 }
